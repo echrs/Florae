adafc7d4705fdfc640a6b676ad8dcb1f46eb5895
nikki <6318495+echrs@users.noreply.github.com>
2022-08-10 11:41:23 +0200

    Update index.tsx

--- api/index.tsx
+++ api/index.tsx
@@ -7,10 +7,6 @@ const api = axios.create({ baseURL: 'https://florae-back.herokuapp.com' });
 export const register = (formData: any) => api.post('/register', formData);
 export const login = (formData: any) => api.post('/login', formData);
 export const logout = async () => {
-  AsyncStorage.getAllKeys().then((keys) => AsyncStorage.multiRemove(keys));
+  AsyncStorage.getAllKeys().then(async (keys) => await AsyncStorage.multiRemove(keys));
 };
-export const savePlant = (formData: any, token: string) => api.post('/plants', formData, { headers: { Authorization: `Bearer ${token}` } });
-export const editPlant = (plantId: string, formData: any, token: string) =>
-  api.patch(`/plants/${plantId}`, formData, { headers: { Authorization: `Bearer ${token}` } });
-export const deletePlant = (plantId: string, token: string) => api.delete(`/plants/${plantId}`, { headers: { Authorization: `Bearer ${token}` } });
-export const getPlants = (token: string) => api.get('/plants', { headers: { Authorization: `Bearer ${token}` } });
+export const syncPlants = (plants: any, token: string) => api.post('/plants', plants, { headers: { Authorization: `Bearer ${token}` } });
